e8c2a4745f544b74eec27703b62556853110cbc0
danielemurgioni <dashomew@gmail.com>
2025-04-14 17:02:37 +0200

    [UPD] import function from hobbies.js

--- js/people.js
+++ js/people.js
@@ -1,8 +1,12 @@
 const names = require("./names.js");
+const hobbies = require("./hobbies.js");
 
-const myName = "Daniele"
-const mySurname = "Murgioni"
+const myHobbies = ["videogiochi", "musica", "pixel art"];
+const myName = "Daniele";
+const mySurname = "Murgioni";
 
-const FullName = names(myName, mySurname)
+const FullName = names(myName, mySurname);
+const funzHobbies = hobbies(myHobbies[0],myHobbies[1],myHobbies[2]);
 
 console.log(`${FullName.firstName} ${FullName.lastName}`);
+console.log(funzHobbies);
